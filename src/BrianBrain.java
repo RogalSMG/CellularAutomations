41f9e6d9b4bf7f36cf1f819a0fbf333773e17a99
Daniel R <danielsmogorzow1@gmail.com>
2023-02-14 11:54:18 +0100

    Brian's Brain changes

--- src/BrianBrain.java
+++ src/BrianBrain.java
@@ -1,18 +1,35 @@
 import java.awt.*;
 
 public class BrianBrain extends Automat {
-
-
+public static Color[] COLORS = {Color.WHITE, Color.BLACK, Color.GRAY}; // new set of colors for Brian's Brain game
+    /**
+     * Standard constructor where dimensions are hardcoded
+     */
     public BrianBrain() {
-        Color[] colors = {Color.WHITE, Color.BLACK, Color.GRAY};
-        Cell.setCOLORS(colors);
-        grid = new GridCanvas(20,20,20);
+        Cell.setCOLORS(COLORS);
+        grid = new GridCanvas(20, 20, 20);
         grid.setGoThroughBorder(true);
-        grid.randomSetState(0,3);
+        grid.randomSetState(0, 3);
     }
 
+    /**
+     * Constructor where size of grid is specified by parameters
+     * @param rows number of rows for grid
+     * @param cols number of columns for grid
+     */
+    public BrianBrain(int rows, int cols) {
+        Cell.setCOLORS(COLORS);
+        grid = new GridCanvas(rows, cols, 20);
+        grid.setGoThroughBorder(true);
+        grid.randomSetState(0, 3);
+    }
+
+    /**
+     * Create and run simulation
+     * @param args command prompt argument
+     */
     public static void main(String[] args) {
-        BrianBrain brianBrain = new BrianBrain();
+        BrianBrain brianBrain = new BrianBrain(53,98);
         brianBrain.run("Brian Game", 5);
 
     }
@@ -26,18 +43,27 @@ public class BrianBrain extends Automat {
         int alive = grid.countState(1);
         int dying = grid.countState(2);
         System.out.println("Alive: " + alive + " Dying: " + dying);
-        cellUpdate(hist);
+        cellUpdate(hist, 2);
 
     }
 
-    private void cellUpdate(int[][] hist) {
+    /**
+     * Method implement rules of Brian's Brain game:
+     * - Dead cells alive if they were surround by {@code aliveCells} amount of alive cell
+     * - Alive cells change to dying
+     * - Dying cells change to dead
+     *
+     * @param hist two dimension int array included amount of cells which are alive in the neighborhood for each {@code cell} object in the {@code this.grid}
+     * @param aliveCells alive cells around specified cell needed to revive this cell
+     */
+    private void cellUpdate(int[][] hist, int aliveCells) {
         for (int y = 0; y < grid.numRows(); y++) {
             for (int x = 0; x < grid.numColumns(); x++) {
                 Cell cell = grid.getCell(y, x);
                 int state = cell.getState();
                 switch (state) {
                     case 0 -> {
-                        if (hist[y][x] == 2) { // if dead cell have exact 2 alive neighbors
+                        if (hist[y][x] == aliveCells) { // if dead cell have exact 2 alive neighbors
                             cell.setState(1); // get alive, set status to 1
                         }
                     }
