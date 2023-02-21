d191343b7b90c0522030395a1a95f301b00991a9
Daniel R <danielsmogorzow1@gmail.com>
2023-02-21 15:17:02 +0100

    Improvements

--- src/WireWorld.java
+++ src/WireWorld.java
@@ -13,20 +13,21 @@ public class WireWorld extends Automat {
      */
     public WireWorld() {
         Cell.setCOLORS(COLORS);
-        grid = new GridCanvas(20,20,20);
-        grid.getCell(4,4).setState(3);
-        grid.getCell(4,5).setState(3);
-        grid.getCell(4,6).setState(2);
-        grid.getCell(5,7).setState(1);
-        grid.getCell(6,6).setState(3);
-        grid.getCell(6,5).setState(3);
-        grid.getCell(6,4).setState(3);
-        grid.getCell(5,3).setState(3);
-        grid.getCell(4,4).setState(3);
+        grid = new GridCanvas(20, 20, 20);
+        grid.getCell(4, 4).setState(3);
+        grid.getCell(4, 5).setState(3);
+        grid.getCell(4, 6).setState(2);
+        grid.getCell(5, 7).setState(1);
+        grid.getCell(6, 6).setState(3);
+        grid.getCell(6, 5).setState(3);
+        grid.getCell(6, 4).setState(3);
+        grid.getCell(5, 3).setState(3);
+        grid.getCell(4, 4).setState(3);
     }
 
     /**
      * Constructor where size of grid are hardCoded
+     *
      * @param rows number of rows
      * @param cols number of cols
      */
@@ -37,14 +38,15 @@ public class WireWorld extends Automat {
 
     /**
      * Create and run simulation
+     *
      * @param args command prompt argument
      */
     public static void main(String[] args) {
-        WireWorld world = new WireWorld(20,30);
-        world.readWWP("E:\\Developer Start\\Java Projekty\\GameOfLife\\src\\wireWorldPatterns\\test.wwp");
+        WireWorld world = new WireWorld(20, 30);
+        world.setStates(world.readWWP("E:\\Developer Start\\Java Projekty\\GameOfLife\\src\\wireWorldPatterns\\XOR_Gate.wwp"));
         world.run("Wire World", 4);
-
     }
+
     /**
      * Method simulate one electron move.
      */
@@ -59,6 +61,7 @@ public class WireWorld extends Automat {
      * - electron head (1) -> electron tail (2)
      * - electron tail (2) -> electron head (1)
      * - conductor (3) -> electron head (1) only if surrounded exact by one or two other electrons
+     *
      * @param electronHist two dimension int array included amount of electron heads cells {@code  cell.getState == 2} around each {@code cell} of grid
      */
     private void cellUpdate(int[][] electronHist) {
@@ -79,31 +82,44 @@ public class WireWorld extends Automat {
         }
     }
 
-    public void readWWP(String path) {
+    /**
+     * Method read file to {@code ArrayList<String>} object and return them.
+     *
+     * @param path given file path
+     */
+    public ArrayList<String> readWWP(String path) {
+        if (!path.endsWith(".wwp")) {
+            throw new IllegalArgumentException();
+        }
         // add file to scanner
         Scanner scan = loadFile(path);
 
         // create arrayList
-        ArrayList<String> list = readFile(scan);
-
-        // set dimension?
-
-        // set states
-        setStates(list);
+        return readFile(scan);
     }
 
+    /**
+     * Method set appropriate state for each cell of the grid according to given {@code ArrayList} param. <p>
+     * 0 - Background
+     * 1 - Electron head
+     * 2 - Electron tail
+     * 3 - Conductor
+     *
+     * @param list given list
+     */
     private void setStates(ArrayList<String> list) {
         for (int y = 0; y < list.size(); y++) {
             String line = list.get(y);
             for (int x = 0; x < line.length(); x++) {
                 int state = Integer.parseInt(String.valueOf(line.charAt(x))); // needed to convert char to string first and then string to int
-                grid.getCell(y,x).setState(state);
+                grid.getCell(y, x).setState(state);
             }
         }
     }
 
     /**
-     * Helper method read file and return arrayList of strings
+     * Helper method read file and return arrayList of strings.
+     *
      * @param scan given Scanner object
      * @return list of strings
      */
@@ -120,12 +136,13 @@ public class WireWorld extends Automat {
 
     /**
      * Helper method which load file under given path to Scanner object
+     *
      * @param path given string with file path
      * @return Scanner object with loaded file if exist
      */
     private Scanner loadFile(String path) {
-        File file  = new File(path);
-        Scanner scan = null;
+        File file = new File(path);
+        Scanner scan;
         try {
             scan = new Scanner(file);
         } catch (FileNotFoundException e) {
@@ -134,7 +151,6 @@ public class WireWorld extends Automat {
         }
         return scan;
     }
-
 
     public static class WireWorldPattern {
 
